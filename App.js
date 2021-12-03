c10dd1fcaa2983caf6a3969cbb91c9b5a35ed41b
falafelpro <47983933+falafelpro@users.noreply.github.com>
2021-12-03 13:01:22 +0300

    File structure is done + some models

---
+++ App.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const connectDB = require("./db/database");
+const morgan = require("morgan");
+const logger = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cors = require("cors");
+const path = require("path");
+const passport = require("passport");
+const { localStrategy, jwtStrategy } = require("./middleware/passport");
+
+const app = express();
+
+connectDB();
+
+// Middleware
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(logger);
+app.use(passport.initialize());
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+app.use(cors());
+
+// Routes
+
+app.use("/assets", express.static(path.join(__dirname, "assets")));
+// add your Routes here
+//app.use("/api", userRoutes);
+
+app.use((req, res, next) =>
+  res.status(404).json({ message: "Path not found" })
+);
+
+app.use(errorHandler);
+
+const PORT = 8000;
+app.listen(PORT, () => console.log(`Application running on localhost:${PORT}`));
---
+++ db/Models/Truck.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const TruckSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    speciality: {
+      type: [String],
+      enum: ["Burger", "Coffee", "Mexican", "Indian"],
+    },
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    dishes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Dish",
+      },
+    ],
+    location: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Location",
+    },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("Truck", TruckSchema);
