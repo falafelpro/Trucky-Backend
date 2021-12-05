67d4bc564ecf5c58277afdc10eaff869672d406a
AliAbdulkarimov94 <alikenbo@gmail.com>
2021-12-05 15:42:04 +0200

    sign in up done

---
+++ api/user /user.controller.js
@@ -0,0 +1,42 @@
+const User = require("../../db/model/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { JWT_EXPIRATION_MS, JWT_SECRET } = require("../../config/keys");
+const Profile = require("../../db/model/Profile");
+
+
+
+const generateToken = (user) => {
+    const payload = {
+        _id: user._id,
+        username: user.username,
+        exp: Date.now() + JWT_EXPIRATION_MS,
+    };
+    
+    const token = jwt.sign(payload, JWT_SECRET);
+    return token;
+       
+    };
+
+exports.signup = async (req, res, next) => {
+    try {
+      const saltRounds = 10;
+      const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+      req.body.password = hashedPassword;
+
+      const newUser = await User.create(req.body);
+      const profile = await Profile.create({user: newUser._id});
+      const token = generateToken(newUser);
+
+
+      res.status(201).json({ token });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  exports.signin = (req, res, next) => {
+      const token = generateToken(req.user);
+      res.json ({ token });
+  };
+
